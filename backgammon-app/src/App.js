ce54f3b97260578c6437b30e4473c371464dcd30
AlexHomePC <gotlib14@gmail.com>
2023-01-21 14:17:05 +0200

    Added "HomePage" + routing to Frontend

--- backgammon-app/src/App.js
+++ backgammon-app/src/App.js
@@ -1,27 +1,39 @@
 import { getAllByText } from '@testing-library/react'
-import React, { useState,useEffect } from 'react'
-import Chat from './components/Chat/Chat';
+import React, { useState, useEffect } from 'react'
+import { Routes,Route } from 'react-router-dom'
+
+import Chat from './components/Chat/Chat'
+import NavBar from './components/NavBar/navbar'
+import Home from './components/Home/home'
+
 
 function App () {
+  const [alex, setAlex] = useState({})
 
-  const [alex,setAlex] = useState({});
-
-  const getAllByText= ()=>{
-    const url = "http://localhost:5032/api/Test"
-    fetch(url,{
-    }).then(response => response.json()).then(postFormServer =>{
-      setAlex(postFormServer);
-      console.log(postFormServer);
-    });
+  const getAllByText = () => {
+    const url = 'http://localhost:5032/api/Test'
+    fetch(url, {})
+      .then(response => response.json())
+      .then(postFormServer => {
+        setAlex(postFormServer)
+        console.log(postFormServer)
+      })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getAllByText()
-  },[])
-  
+  }, [])
+
   return (
-    <div >
-      <Chat />
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route path="/" exact element={<Home/>}></Route>
+        <Route path="/startgame" exact element={<Home/>}></Route>
+        <Route path="/friends" exact element={<Home/>}></Route>
+        <Route path="/rules" exact element={<Home/>}></Route>
+      </Routes>
+
     </div>
   )
 }
---
+++ backgammon-app/src/components/NavBar/navbar.js
@@ -0,0 +1,40 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'react-router-dom'
+
+import Figure from 'react-bootstrap/Figure'
+
+function NavBar () {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand to='/'>BACKGAMMON</Navbar.Brand>
+          <div className='d-flex'>
+            <img
+              className='profileImg'
+              src='https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'
+              alt='user'
+            />
+            <h3>Name</h3>
+          </div>
+          <Nav className='justify-content-end'>
+            <NavLink className='nav-link' to='/rules'>
+              Rules
+            </NavLink>
+            <NavLink className='nav-link' to='/'>
+              Home
+            </NavLink>
+            <NavLink className='nav-link' to='/friends'>
+              Friends
+            </NavLink>
+            <NavLink className='nav-link'>Log In</NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default NavBar
